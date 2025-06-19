757abd92480b183088f0e079949cca374562253c
Alexis De Chiara <alexis.de-chiara@hotmail.fr>
2025-06-19 17:16:19 +0200

    🐛 Petite correction de fetchPlayer pour les settings

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,11 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
 	const user = useSupabaseUser()
+	const { fetchPlayer, getPlayer } = usePlayerStore()
 
 	if (!user.value) {
 		return navigateTo("/register")
+	}
+	else if (getPlayer == null) {
+		fetchPlayer()
 	}
 })
